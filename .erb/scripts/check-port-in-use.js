dc81c0557b5f7535876ae9fed903bf4a868581f8
Eduardo <81358190+CyberXzEdu@users.noreply.github.com>
2023-07-11 16:10:32 -0300

    Update check-port-in-use.js

--- .erb/scripts/check-port-in-use.js
+++ .erb/scripts/check-port-in-use.js
@@ -1,15 +1,23 @@
-import chalk from 'chalk';
-import detectPort from 'detect-port';
+// import chalk from "chalk";
+import { exec } from "child_process";
+import detectPort from "detect-port";
 
-const port = process.env.PORT || '1212';
+const port = process.env.PORT || "1212";
 
 detectPort(port, (err, availablePort) => {
   if (port !== String(availablePort)) {
-    throw new Error(
+    console.log(`Port "${port}" is already in use. Killing it...`);
+    exec("npm run kill", (error, stdout) => {
+      if (err) throw new Error(error);
+      if (stdout) console.log(stdout);
+      process.exit(0);
+    });
+
+    /* throw new Error(
       chalk.whiteBright.bgRed.bold(
         `Port "${port}" on "localhost" is already in use. Please use another port. ex: PORT=4343 npm start`
       )
-    );
+    ); */
   } else {
     process.exit(0);
   }
